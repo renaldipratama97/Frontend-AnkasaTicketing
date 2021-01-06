4d30b8fa247aa9d4150ccfbc2b0632be7d9863b9
sjasminetya <shaulajasminetya22@gmail.com>
2021-01-06 19:09:04 +0700

    login page

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Vuelidate from 'vuelidate'
 
 Vue.config.productionTip = false
+Vue.use(Vuelidate)
 
 new Vue({
   router,
---
+++ src/views/auth/Auth.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-7 col-md-7 d-none d-sm-block bg">
+                <div class="icon-ankasa text-center">
+                    <img src="../../assets/illustration.png" alt="background">
+                </div>
+            </div>
+            <div class="col-lg-5 col-md-5">
+                <router-view/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Auth'
+}
+</script>
+
+<style scoped>
+.bg {
+    background: #2395FF;
+    height: fit-content;
+}
+
+.bg .icon-ankasa img {
+    margin: 200px 0;
+}
+
+@media (max-width: 799px) {
+    .bg .icon-ankasa img {
+        margin-left: -30px;
+    }
+}
+
+@media (max-width: 775px) {
+    .bg .icon-ankasa img {
+        margin-left: -40px;
+    }
+}
+</style>
